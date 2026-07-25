ac66cf824ae21e4eadd122f778b6d228ee2179b1
Reza Luna 2501091005 <rezaluna2313@gmail.com>
2026-07-25 16:50:55 +0700

    Update latihan7_2b.java

--- latihan7_2b.java
+++ latihan7_2b.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Adellia020426;
+package rza240226;
 import javax.swing.JOptionPane;
 
 
