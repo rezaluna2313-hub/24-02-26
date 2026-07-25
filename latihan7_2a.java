74b533a51bf1d13d3cd971924ff4f3f145ef67e1
Reza Luna 2501091005 <rezaluna2313@gmail.com>
2026-07-25 16:50:37 +0700

    Update latihan7_2a.java

--- latihan7_2a.java
+++ latihan7_2a.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Adellia020426;
+package rza240226;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
